7cdd26085ce00136201031bcfda517fc174c4a3a
cangxue <752787086@qq.com>
2017-08-12 23:30:03 +0800

    生成二维码类文件添加

---
+++ CXQRCode/CXQRCode/CXQRCode/CXQRCodeGenerateManager.h
@@ -0,0 +1,13 @@
+//
+//  CXQRCodeGenerateManager.h
+//  CXQRCode
+//
+//  Created by xiaoma on 2017/8/12.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CXQRCodeGenerateManager : NSObject
+
+@end
---
+++ CXQRCode/CXQRCode/CXQRCode/CXQRCodeGenerateManager.m
@@ -0,0 +1,13 @@
+//
+//  CXQRCodeGenerateManager.m
+//  CXQRCode
+//
+//  Created by xiaoma on 2017/8/12.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import "CXQRCodeGenerateManager.h"
+
+@implementation CXQRCodeGenerateManager
+
+@end
