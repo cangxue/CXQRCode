5aa01c6abd462127b62ea7f7ef87036786ea5326
cangxue <752787086@qq.com>
2017-08-13 23:31:42 +0800

    生成带logo的二维码

--- CXQRCode/CXQRCode/CXQRCode/CXQRCodeGenerateManager.h
+++ CXQRCode/CXQRCode/CXQRCode/CXQRCodeGenerateManager.h
@@ -14,4 +14,7 @@
 // 生成普通二维码
 + (UIImage *)CX_generateDefaultQRCodeWithData:(NSString *)data imageWidth:(CGFloat)imageWidth;
 
+// 生成一张带有logo的二维码（logoScaleToSuperView：相对于父视图的缩放比取值范围0-1；0，不显示，1，代表与父视图大小相同）
++ (UIImage *)CX_generateWithLogoQRCodeData:(NSString *)data logoImageName:(NSString *)logoImageName logoScaleToSuperView:(CGFloat)logoScaleToSuperView;
+
 @end
